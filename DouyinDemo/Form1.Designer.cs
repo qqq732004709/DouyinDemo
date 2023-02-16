e0ea03bed0c227c4db504ff1e6021b46fcc07d09
Xiaoyu <732004709@qq.com>
2023-02-16 10:17:31 +0800

    添加项目文件。

---
+++ DouyinDemo/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace DouyinDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.container = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.goButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.container.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // container
+            // 
+            this.container.Controls.Add(this.button3);
+            this.container.Controls.Add(this.button2);
+            this.container.Controls.Add(this.goButton);
+            this.container.Controls.Add(this.button1);
+            this.container.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.container.Location = new System.Drawing.Point(0, 0);
+            this.container.Name = "container";
+            this.container.Size = new System.Drawing.Size(1018, 534);
+            this.container.TabIndex = 0;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(383, 9);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(118, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "上传视频";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(261, 9);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "加载cookie";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // goButton
+            // 
+            this.goButton.Location = new System.Drawing.Point(139, 9);
+            this.goButton.Name = "goButton";
+            this.goButton.Size = new System.Drawing.Size(116, 23);
+            this.goButton.TabIndex = 1;
+            this.goButton.Text = "清除cookie";
+            this.goButton.UseVisualStyleBackColor = true;
+            this.goButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(22, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "保存cookie";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1018, 534);
+            this.Controls.Add(this.container);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.container.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel container;
+        private System.Windows.Forms.Button goButton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ DouyinDemo/Form1.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.Playwright;
+using Microsoft.Web.WebView2.Core;
+using Microsoft.Web.WebView2.WinForms;
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Windows.Forms;
+
+namespace DouyinDemo
+{
+    public partial class Form1 : Form
+    {
+        IBrowser browser;
+        IBrowserContext context;
+        IPage page;
+        IPlaywright playwright;
+        WebView2 webView;
+
+        public string CookieInfo { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            //动态加载webView2 并开启CDP控制端口
+            webView = new WebView2
+            {
+                Visible = true,
+                Dock = DockStyle.Fill,
+            };
+            container.Controls.Add(webView);
+            await webView.EnsureCoreWebView2Async(await CoreWebView2Environment.CreateAsync(null, null, new CoreWebView2EnvironmentOptions()
+            {
+                AdditionalBrowserArguments = "--remote-debugging-port=9223",
+            })).ConfigureAwait(true);
+
+            //通过playwright跳转douyin.com
+            playwright = await Playwright.CreateAsync();
+            browser = await playwright.Chromium.ConnectOverCDPAsync("http://localhost:9223");
+            context = browser.Contexts[0];
+            page = context.Pages[0];
+            await page.GotoAsync("https://creator.douyin.com");
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            //清除缓存 重新加载当前页面
+            await context.ClearCookiesAsync();
+            await page.GotoAsync("https://www.douyin.com");
+        }
+
+        private async void button1_Click_1(object sender, EventArgs e)
+        {
+            //保存cookie到内存中
+            var authInfo = await context.CookiesAsync();
+            var cookieJson = JsonSerializer.Serialize(authInfo);
+            CookieInfo = cookieJson;
+            MessageBox.Show("保存成功");
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            //加载cookie
+            var cookieList = JsonSerializer.Deserialize<List<Microsoft.Playwright.Cookie>>(CookieInfo);
+            if (cookieList != null && cookieList.Count > 0)
+            {
+                await context.AddCookiesAsync(cookieList);
+            }
+            await page.GotoAsync("https://www.douyin.com");
+        }
+
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            await page.GetByRole(AriaRole.Button, new PageGetByRoleOptions() { Name = "发布作品" }).ClickAsync();
+            
+            await page.Locator(".upload-btn-input--1NeEX").SetInputFilesAsync(new[] { @"C:\Users\xy\Desktop\20230215-190642.mp4" });
+            //标题
+            await page.Locator(".zone-container").FillAsync("这是个测试标题​");
+
+            //视频分类
+            await page.Locator("div.semi-cascader-selection").ClickAsync();
+            await page.GetByRole(AriaRole.Listitem).Filter(new LocatorFilterOptions { HasText = "电视剧" }).ClickAsync();
+            await page.GetByText("电视剧解说").ClickAsync();
+
+            //视频标签
+            await page.GetByPlaceholder("输入后按 Enter 键可添加自定义标签").FillAsync("测试标签");
+            await page.GetByPlaceholder("输入后按 Enter 键可添加自定义标签").PressAsync("Enter");
+
+            //申请关联热点
+            await page.GetByText("点击输入热点词").ClickAsync();
+            await page.GetByText("美国公然喊话中国对伊朗施压").ClickAsync();
+
+            //允许他人保存视频
+            await page.Locator("label").Filter(new LocatorFilterOptions  { HasText = "不允许" }).ClickAsync();
+            //设置谁可以看
+            await page.Locator("label").Filter(new LocatorFilterOptions { HasText = "好友可见" }).ClickAsync();
+
+            await page.GetByRole(AriaRole.Button, new PageGetByRoleOptions { Name = "发布", Exact = true }).ClickAsync();
+            await page.GetByRole(AriaRole.Button, new PageGetByRoleOptions { Name = "暂不同步" }).ClickAsync();
+        }
+    }
+}

8f045ebb6da06c791a8bafdae35a3b61664a9791
Xiaoyu <732004709@qq.com>
2023-02-17 23:30:29 +0800

    长视频上传失败检测逻辑

--- DouyinDemo/Form1.cs
+++ DouyinDemo/Form1.cs
@@ -75,8 +75,31 @@ namespace DouyinDemo
         private async void button3_Click(object sender, EventArgs e)
         {
             await page.GetByRole(AriaRole.Button, new PageGetByRoleOptions() { Name = "发布作品" }).ClickAsync();
-            
-            await page.Locator(".upload-btn-input--1NeEX").SetInputFilesAsync(new[] { @"C:\Users\xy\Desktop\20230215-190642.mp4" });
+
+            await page.Locator(".upload-btn-input--1NeEX").SetInputFilesAsync(new[] { @"C:\Users\xy\Documents\WeChat Files\wxid_p2q9v7slvazj21\FileStorage\File\2023-02\6分钟视频-第二版3种音乐\6分钟视频-第二版3种音乐.mp4" });
+
+            //循环检测视频是否上传成功
+            var loopCount = 0;
+            while (true)
+            {
+                try
+                {
+                    loopCount++;
+                    if (loopCount == 50)
+                    {
+                        MessageBox.Show("上传失败");
+                        return;
+                    }
+                    await page.GetByText("重新上传").WaitForAsync(new LocatorWaitForOptions { State = WaitForSelectorState.Visible });
+                }
+                catch (Exception ex)
+                {
+                    //未上传成功等待1秒继续
+                    await page.WaitForTimeoutAsync(1000);
+                    continue;
+                }
+            }
+
             //标题
             await page.Locator(".zone-container").FillAsync("这是个测试标题​");
 
@@ -88,13 +111,13 @@ namespace DouyinDemo
             //视频标签
             await page.GetByPlaceholder("输入后按 Enter 键可添加自定义标签").FillAsync("测试标签");
             await page.GetByPlaceholder("输入后按 Enter 键可添加自定义标签").PressAsync("Enter");
-
+            await page.PauseAsync();
             //申请关联热点
             await page.GetByText("点击输入热点词").ClickAsync();
             await page.GetByText("美国公然喊话中国对伊朗施压").ClickAsync();
 
             //允许他人保存视频
-            await page.Locator("label").Filter(new LocatorFilterOptions  { HasText = "不允许" }).ClickAsync();
+            await page.Locator("label").Filter(new LocatorFilterOptions { HasText = "不允许" }).ClickAsync();
             //设置谁可以看
             await page.Locator("label").Filter(new LocatorFilterOptions { HasText = "好友可见" }).ClickAsync();
 
